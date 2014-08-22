be94553021e27868b867c36dda7d0d4266afa93e
iyadassaf <iyadassaflondon@gmail.com>
2014-08-22 22:55:43 +0100

    Working configuration and profile selecting

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 causeway
 =====
 
+[![wercker status](https://app.wercker.com/status/0d7d2adc026498b195f564f23864616a/s "wercker status")](https://app.wercker.com/project/bykey/0d7d2adc026498b195f564f23864616a)
+
 - A controlled & monitored job queue spanning between an api server and a client
 
 Features:
--- examples/usage.js
+++ examples/usage.js
@@ -6,21 +6,23 @@
      * - Register to a redis pubsub bridge
      * - Send jobs and recieve results
      */
-    var causeway = new require('causeway')();
+    var causeway = new (require('../index.js'))();
 
     /**
      * @method register.proc
      * @desc register redis processing addresses
      */
     causeway.register({
-        port: 1232,
-        address: 0.0.0.0,
-        auth: '62767sd8fs'
+        default: {
+            "port": 6379,
+            "addr": "127.0.0.1"
+        }
     });
+    
+    // console.log(causeway._connections);
+    // console.log(causeway._connections['default']);
 
-
-
-    causeway.send('load_user', { some: 'args' }).then(function (result) {
+    causeway.using('default').request('load_user', { some: 'args' }).then(function (result) {
         console.log('Result of job: ' + JSON.stringify(result, null, 4));
     });
 
